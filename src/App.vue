945a996fe7979eb4612436cd58eb574117e236d1
TALEX1995 <tanase.alex@libero.it>
2023-09-11 17:03:38 +0200

    Added axios request

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,26 @@
 <script>
 import axios from 'axios';
-const endpoint = 'http://127.0.0.1:8000/api/projects';
+const endpoint = 'http://127.0.0.1:8000/api/projects/';
 import AppHeader from './components/AppHeader.vue';
 export default {
-  components: { AppHeader }
+  // Components
+  components: { AppHeader },
+  // Data
+  data: () => ({
+    projects: [],
+  }),
+  // Methods
+  methods: {
+    fetchProjects() {
+      axios.get(endpoint).then(res => {
+        console.log(res.data)
+      })
+    }
+  },
+  // Created
+  created() {
+    this.fetchProjects();
+  }
 };
 </script>
 
